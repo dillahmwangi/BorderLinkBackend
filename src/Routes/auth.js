15edb574ca778f2374e814a4f874caab82e12861
Josphat Mwangi <josphatwanjiruw@gmail.com>
2024-03-21 17:18:34 +0300

    create auth control and route
    remove controller from route and update controller

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -1,50 +1,64 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../Model/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const authController = require('../Controllers/authController')
+const jwt = require('jsonwebtoken');
+const User = require('../Model/user');
 
 
 
 // User registration
-router.post('/register', async (req, res) => {
-    try {
-    const { userName, password,email, phone,country, idNo} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ userName, password: hashedPassword, email,phone, country, idNo });
+router.post('/register', authController.register)
+router.post('/login', authController.login)
+router.post('/logout', authController.logout)
+router.post('/forgotPassword', authController.forgotPassword);
+router.put('/resetPassword', authController.resetPassword);
 
-    await user.save();
-    res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Registration failed'+ error});
-    }
-    });
 
-    // User login
- router.post('/login', async (req, res) => {
-    try {
-    const { userName, password } = req.body;
-    const user = await User.findOne({ userName:userName });
-    if (!user) {
-    return res.status(401).json({ error: 'Authentication failed' });
-    }
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-    return res.status(401).json({ error: 'Authentication failed' });
-    }
-    const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
-    expiresIn: '1h',
-    });
-    res.status(200).json({ token });
-    } catch (error) {
-    res.status(500).json({ error: 'Login failed' });
-    }
-    });
-    
-    router.post('/forgotPassword', authController.forgotPassword);
-    router.put('/resetPassword/:resetToken', authController.resetPassword)
-     ;
-   
-   module.exports = router;
+const verifyToken = (req, res, next) => {
+  const token_with_bearer = req.headers['authorization'];
+  const token = token_with_bearer.split(' ')[1]
+
+  if (!token) {
+      console.log("am token: ", token)
+      return res.status(401).json({ message: 'Authorization token not provided' });
+  }
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).json({ message: 'Invalid token' });
+      }
+      req.userId = decoded.user_id; // Attach user ID to request object
+      next();
+  });
+};
+
+// GET user profile
+router.get('/profile', verifyToken, async (req, res) => {
+  const userId = req.userId;
+
+  try {
+      // Fetch user profile from the database based on userId
+      const user = await User.findById(userId);
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
+
+      // Send user profile as a JSON response
+      res.status(200).json({
+          username: user.username,
+          email: user.email,
+          phone: user.phone,
+          country: user.country,
+          idNo: user.idNo,
+          createdAt: user.createdAt
+          // Add other profile fields as needed
+      });
+  } catch (error) {
+      console.error('Error fetching user profile:', error);
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+     
+module.exports = router;
